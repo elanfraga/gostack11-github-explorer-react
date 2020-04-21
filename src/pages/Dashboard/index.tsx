17be8fb5eb3298ef30a5856840b3ff60bc6e65b0
Elan Fraga <elanfraga@gmai.com>
2020-04-21 01:06:45 -0300

    Conectando a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,64 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, From, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github.</Title>
 
-      <From>
-        <input placeholder="Digite o nome do repositório" />
+      <From onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </From>
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/25745452?s=400&u=875f906f1d54527f5bc0f2e278c1ff7b0793df14&v=4"
-            alt="Elan Fraga"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Lorem Ipsum has been the industry's standard dummy text</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/25745452?s=400&u=875f906f1d54527f5bc0f2e278c1ff7b0793df14&v=4"
-            alt="Elan Fraga"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Lorem Ipsum has been the industry's standard dummy text</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/25745452?s=400&u=875f906f1d54527f5bc0f2e278c1ff7b0793df14&v=4"
-            alt="Elan Fraga"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Lorem Ipsum has been the industry's standard dummy text</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
