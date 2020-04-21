1c2bd66d554b70b41ff183b1f2fb20c71e28312f
Elan Fraga <elanfraga@gmai.com>
2020-04-21 00:26:43 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, From, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <From>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </From>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/25745452?s=400&u=875f906f1d54527f5bc0f2e278c1ff7b0793df14&v=4"
+            alt="Elan Fraga"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Lorem Ipsum has been the industry's standard dummy text</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/25745452?s=400&u=875f906f1d54527f5bc0f2e278c1ff7b0793df14&v=4"
+            alt="Elan Fraga"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Lorem Ipsum has been the industry's standard dummy text</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/25745452?s=400&u=875f906f1d54527f5bc0f2e278c1ff7b0793df14&v=4"
+            alt="Elan Fraga"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Lorem Ipsum has been the industry's standard dummy text</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
