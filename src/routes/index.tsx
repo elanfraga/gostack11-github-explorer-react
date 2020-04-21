c7a6fdaf90360bfcdb23182e479ebc56627d0e59
Elan Fraga <elanfraga@gmai.com>
2020-04-20 22:33:56 -0300

    Criando Rotas

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashoborad from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashoborad} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
