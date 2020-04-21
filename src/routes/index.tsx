ae4e0d4523c3dbd937542861c0d1a121d8ec25d9
Elan Fraga <elanfraga@gmai.com>
2020-04-21 15:14:35 -0300

    Navegando entre rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import Repository from '../pages/Repository';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashoborad} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
 );
 
